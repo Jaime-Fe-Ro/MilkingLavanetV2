1fd0dd761542dbaa4902411048829c2315dfb6a8
Jaime Fernández <jaime.fe.jb@gmail.com>
2024-03-05 15:55:27 +0100

    wallet index bug fixed

--- main.py
+++ main.py
@@ -263,26 +263,26 @@ async def fetch_data(session, payload, rpc_endpoint, wallet_address, wallet_inde
     try:
         async with session.post(rpc_endpoint, json=payload) as response:
             if response.status == 429:
-                print(f"ERROR. Too Many Requests. Waiting 1 second for the server to recover. {wallet_index}: {wallet_address}")
+                print(f"ERROR. Too Many Requests. Waiting 1 second for the server to recover. {wallet_index + 1}: {wallet_address}")
                 await asyncio.sleep(1)
                 return await fetch_data(session, payload, rpc_endpoint, wallet_address, wallet_index)
 
             if response.status != 200:
-                print(f"ERROR. Error fetching data: HTTP status {response.status}. {wallet_index}: {wallet_address}")
+                print(f"ERROR. Error fetching data: HTTP status {response.status}. {wallet_index + 1}: {wallet_address}")
                 return await fetch_data(session, payload, rpc_endpoint, wallet_address, wallet_index)
 
             content_type = response.headers.get('content-type', '').lower()
             if 'application/json' not in content_type:
-                print(f"ERROR. Unexpected response content type: {content_type}, {wallet_index}: {wallet_address}")
+                print(f"ERROR. Unexpected response content type: {content_type}, {wallet_index + 1}: {wallet_address}")
                 return await fetch_data(session, payload, rpc_endpoint, wallet_address, wallet_index)
 
             return await response.json()
     except ClientOSError:
-        print(f"ERROR. A network error occurred. Retrying after 1 second. {wallet_index}: {wallet_address}")
+        print(f"ERROR. A network error occurred. Retrying after 1 second. {wallet_index + 1}: {wallet_address}")
         await asyncio.sleep(1)
         return await fetch_data(session, payload, rpc_endpoint, wallet_address, wallet_index)
     except Exception as e:
-        print(f"ERROR. Error fetching data: {e}. {wallet_index}: {wallet_address}")
+        print(f"ERROR. Error fetching data: {e}. {wallet_index + 1}: {wallet_address}")
         return await fetch_data(session, payload, rpc_endpoint, wallet_address, wallet_index)
 
 
